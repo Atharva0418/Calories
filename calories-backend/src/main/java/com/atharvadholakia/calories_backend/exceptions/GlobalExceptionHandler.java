3ea76fc32ace3f75855974840669091e2c163484
Atharva0418 <atharvadholakia2004@gmail.com>
2025-06-24 17:36:11 +0530

    Added logs and an api for health check.

--- calories-backend/src/main/java/com/atharvadholakia/calories_backend/exceptions/GlobalExceptionHandler.java
+++ calories-backend/src/main/java/com/atharvadholakia/calories_backend/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.atharvadholakia.calories_backend.exceptions;
 
 import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,58 +11,61 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
-  @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<HashMap<String, String>> handleResourceNotFoundException(
-      ResourceNotFoundException ex) {
-    return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
-  }
 
   @ExceptionHandler(MissingServletRequestPartException.class)
   public ResponseEntity<HashMap<String, String>> handleMissingServletRequestPartExcpetion(
       MissingServletRequestPartException ex) {
-    return buildErrorResponse("Please check the correct key parameter.", HttpStatus.BAD_REQUEST);
+    String errorMessage = "Please check the correct key parameter.";
+    log.warn(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(InvalidImageException.class)
   public ResponseEntity<HashMap<String, String>> handleInvalidImageException(
       InvalidImageException ex) {
-    return buildErrorResponse(
-        "Only image(png, jpg/jpeg) files are allowed.", HttpStatus.BAD_REQUEST);
+    String errorMessage = "Only image(png, jpg/jpeg) files are allowed.";
+    log.warn(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(HttpServerErrorException.class)
   public ResponseEntity<HashMap<String, String>> handleHttpServerErrorException(
       HttpServerErrorException ex) {
-    return buildErrorResponse(
-        "Service Unavailable.Please try again later.", HttpStatus.SERVICE_UNAVAILABLE);
+    String errorMessage = "Service Unavailable. Please try again later.";
+    log.error(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.SERVICE_UNAVAILABLE);
   }
 
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public ResponseEntity<HashMap<String, String>> handleMaxUploadSizeExceedException(
       MaxUploadSizeExceededException ex) {
-    return buildErrorResponse(
-        "Image size exceeds the maximum limit of 5 MB", HttpStatus.PAYLOAD_TOO_LARGE);
+    String errorMessage = "Image size exceeds the maximum limit of 5 MB.";
+    log.warn(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.PAYLOAD_TOO_LARGE);
   }
 
   @ExceptionHandler(CustomTimeOutException.class)
   public ResponseEntity<HashMap<String, String>> handleCustomTimeOutException(
       CustomTimeOutException ex) {
-    return buildErrorResponse(
-        "Request taking too long. Please try again later.", HttpStatus.GATEWAY_TIMEOUT);
+    String errorMessage = "Request taking too long. Please try again later.";
+    log.error(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.GATEWAY_TIMEOUT);
   }
 
   @ExceptionHandler(NotAFoodImageException.class)
   public ResponseEntity<?> handleNotAFoodImageExcpetion(NotAFoodImageException ex) {
-    return buildErrorResponse(
-        "This is not a food image. Please upload an appropriate image.", HttpStatus.BAD_REQUEST);
+    String errorMessage = "This is not a food image. Please upload an appropriate image.";
+    log.warn(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<HashMap<String, String>> handleGenericException(Exception ex) {
-    return buildErrorResponse(
-        "Internal Server Error. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    String errorMessage = "Internal Server Error. Please try again later.";
+    log.error(errorMessage);
+    return buildErrorResponse(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   private ResponseEntity<HashMap<String, String>> buildErrorResponse(
