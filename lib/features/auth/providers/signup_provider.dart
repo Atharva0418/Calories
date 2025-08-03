05740128aa2cc400001344d678da2a4c8d5ddfec
Atharva0418 <atharvadholakia2004@gmail.com>
2025-08-03 20:13:28 +0530

    Added API call

--- lib/features/auth/providers/signup_provider.dart
+++ lib/features/auth/providers/signup_provider.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:calories/features/auth/models/signup_request.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class SignupProvider with ChangeNotifier {
   bool _isLoading = false;
@@ -15,9 +19,24 @@ class SignupProvider with ChangeNotifier {
     notifyListeners();
 
     try {
-      await Future.delayed(const Duration(seconds: 2));
-      //TODO: API
+      final url = Uri.parse('${dotenv.env['BASE_URL']}/api/signup');
 
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'name': request.name,
+          'email': request.email,
+          'password': request.password,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print("Signup successful");
+      } else {
+        final data = jsonDecode(response.body);
+        _errorMessage = data['error'] ?? "Signup Failed";
+      }
       if (request.email == 'fail@example.com') {
         throw Exception('Signup failed.');
       }
