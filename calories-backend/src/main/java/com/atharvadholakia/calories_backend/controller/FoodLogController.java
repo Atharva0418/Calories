ebd28e2be5c8b65a879f36d5446a1134fb7c8d16
Atharva0418 <atharvadholakia2004@gmail.com>
2025-09-29 20:30:18 +0530

    Added POST endpoint which saves food logs to db

---
+++ calories-backend/src/main/java/com/atharvadholakia/calories_backend/controller/FoodLogController.java
@@ -0,0 +1,31 @@
+package com.atharvadholakia.calories_backend.controller;
+
+import com.atharvadholakia.calories_backend.data.foodlog.FoodLog;
+import com.atharvadholakia.calories_backend.service.FoodLogService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+@Slf4j
+public class FoodLogController {
+
+  private final FoodLogService foodLogService;
+
+  public FoodLogController(FoodLogService foodLogService) {
+    this.foodLogService = foodLogService;
+  }
+
+  @PostMapping("/addFood")
+  public ResponseEntity<FoodLog> addFoodLog(@RequestBody FoodLog foodLog) {
+    FoodLog savedFoodLog = foodLogService.addFoodLog(foodLog);
+
+    log.info("Food logged successfully.");
+    return new ResponseEntity<>(savedFoodLog, HttpStatus.CREATED);
+  }
+}
