f6789a00f55f978471ae37e2420a0ddb719005bf
Atharva0418 <atharvadholakia2004@gmail.com>
2025-10-29 22:12:28 +0530

    Optimized homescreen: Removed Consumer<Provider> from homescreen to avoiud rebuilding the whole screen upon each state change.

--- lib/features/home_screen.dart
+++ lib/features/home_screen.dart
@@ -81,108 +81,105 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context) {
     final authProvider = context.read<AuthProvider>();
     final chatProvider = context.read<ChatProvider>();
-    return Consumer<NutritionProvider>(
-      builder: (context, provider, child) {
-        if (provider.isLoading) {
-          return const LoadingView();
-        } else {
-          return Scaffold(
-            backgroundColor: Colors.white,
-            body: SafeArea(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(
+    final nutritionProvider = context.watch<NutritionProvider>();
+
+    if (nutritionProvider.isLoading) {
+      return const LoadingView();
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    top: 80.h,
+                    left: -12.w,
+                    child: Transform.scale(
+                      scale: 0.7,
+                      alignment: Alignment.topLeft,
+                      child: SlideTransition(
+                        position: _leftSlide,
+                        child: Image.asset(
+                          'assets/images/dot_grid.png',
+                          color: Colors.indigoAccent,
+                          fit: BoxFit.none,
+                          alignment: Alignment.topLeft,
+                          opacity: AlwaysStoppedAnimation(0.2),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 50.h,
+                      left: 30.w,
+                      right: 20.w,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Positioned(
-                          top: 80.h,
-                          left: -12.w,
-                          child: Transform.scale(
-                            scale: 0.7,
-                            alignment: Alignment.topLeft,
-                            child: SlideTransition(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SlideTransition(
                               position: _leftSlide,
-                              child: Image.asset(
-                                'assets/images/dot_grid.png',
-                                color: Colors.indigoAccent,
-                                fit: BoxFit.none,
-                                alignment: Alignment.topLeft,
-                                opacity: AlwaysStoppedAnimation(0.2),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Colors.white.withValues(alpha: 0.0),
+                                      Colors.blue.withValues(alpha: 0.1),
+                                    ],
+                                  ),
+                                ),
+                                child: Text(
+                                  "Hey ${authProvider.username},",
+                                  style: GoogleFonts.nunito(
+                                    fontSize: 30.sp,
+                                    fontWeight: FontWeight.w900,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            top: 50.h,
-                            left: 30.w,
-                            right: 20.w,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  SlideTransition(
-                                    position: _leftSlide,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                          begin: Alignment.topCenter,
-                                          end: Alignment.bottomCenter,
-                                          colors: [
-                                            Colors.white.withValues(alpha: 0.0),
-                                            Colors.blue.withValues(alpha: 0.1),
-                                          ],
-                                        ),
-                                      ),
-                                      child: Text(
-                                        "Hey ${authProvider.username},",
-                                        style: GoogleFonts.nunito(
-                                          fontSize: 30.sp,
-                                          fontWeight: FontWeight.w900,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
 
-                                  SlideTransition(
-                                    position: _rightSlide,
-                                    child: Image.asset(
-                                      'assets/images/calories_bot.gif',
-                                      height: 140.h,
-                                      fit: BoxFit.contain,
-                                    ),
-                                  ),
-                                ],
+                            SlideTransition(
+                              position: _rightSlide,
+                              child: Image.asset(
+                                'assets/images/calories_bot.gif',
+                                height: 140.h,
+                                fit: BoxFit.contain,
                               ),
+                            ),
+                          ],
+                        ),
 
-                              SizedBox(height: 20.h),
+                        SizedBox(height: 20.h),
 
-                              SlideTransition(
-                                position: _leftSlide,
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Spotted a snack?",
-                                      style: GoogleFonts.dynaPuff(
-                                        fontSize: 28.sp,
-                                        fontWeight: FontWeight.w500,
-                                        color: Color(0xFF9FA8DA), // muted blue
-                                      ),
-                                    ),
-                                    Text(
-                                      "Snap it! or Track it!",
-                                      style: GoogleFonts.dynaPuff(
-                                        fontSize: 28.sp,
-                                        fontWeight: FontWeight.w500,
-                                        color: Color(0xFF9FA8DA),
-                                      ),
-                                    ),
-                                  ],
+                        SlideTransition(
+                          position: _leftSlide,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Spotted a snack?",
+                                style: GoogleFonts.dynaPuff(
+                                  fontSize: 28.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF9FA8DA), // muted blue
+                                ),
+                              ),
+                              Text(
+                                "Snap it! or Track it!",
+                                style: GoogleFonts.dynaPuff(
+                                  fontSize: 28.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF9FA8DA),
                                 ),
                               ),
                             ],
@@ -190,119 +187,117 @@ class _HomeScreenState extends State<HomeScreen>
                         ),
                       ],
                     ),
-
-                    SizedBox(height: 100.h),
-
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SlideTransition(
-                                position: _leftSlide,
-                                child: SnapCard(),
-                              ),
-
-                              SlideTransition(
-                                position: _rightSlide,
-                                child: ChatCard(),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20.h),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SlideTransition(
-                                position: _leftSlide,
-                                child: const LogCard(),
-                              ),
-
-                              SlideTransition(
-                                position: _rightSlide,
-                                child: const HistoryCard(),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            bottomNavigationBar: BottomAppBar(
-              color: Colors.white,
-              padding: EdgeInsets.zero,
-              elevation: 0,
-              height: 60.h,
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border(
-                    top: BorderSide(color: Colors.grey.withValues(alpha: 0.5)),
                   ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                ],
+              ),
+
+              SizedBox(height: 100.h),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
                   children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [FunFactButton(), Text("Trivia")],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SlideTransition(
+                          position: _leftSlide,
+                          child: SnapCard(),
+                        ),
+
+                        SlideTransition(
+                          position: _rightSlide,
+                          child: ChatCard(),
+                        ),
+                      ],
                     ),
-                    InkWell(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder:
-                              (ctx) => AlertDialog(
-                                title: Center(child: Text("Logout")),
-                                content: Text(
-                                  "Are you sure you want to logout?",
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(ctx),
-                                    child: Text("Cancel"),
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () async {
-                                      await authProvider.logout();
-                                      chatProvider.clearChat();
-                                      if (!context.mounted) return;
-                                      Navigator.of(context).pushAndRemoveUntil(
-                                        MaterialPageRoute(
-                                          builder: (_) => LoginScreen(),
-                                        ),
-                                        (route) => false,
-                                      );
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.red,
-                                      foregroundColor: Colors.white,
-                                    ),
-                                    child: Text("Logout"),
-                                  ),
-                                ],
-                              ),
-                        );
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [Icon(Icons.logout_outlined), Text("Logout")],
-                      ),
+                    SizedBox(height: 20.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SlideTransition(
+                          position: _leftSlide,
+                          child: const LogCard(),
+                        ),
+
+                        SlideTransition(
+                          position: _rightSlide,
+                          child: const HistoryCard(),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        padding: EdgeInsets.zero,
+        elevation: 0,
+        height: 60.h,
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey.withValues(alpha: 0.5)),
             ),
-          );
-        }
-      },
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [FunFactButton(), Text("Trivia")],
+              ),
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder:
+                        (ctx) => AlertDialog(
+                          title: Center(child: Text("Logout")),
+                          content: Text("Are you sure you want to logout?"),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.pop(ctx),
+                              child: Text("Cancel"),
+                            ),
+                            ElevatedButton(
+                              onPressed: () async {
+                                await authProvider.logout();
+                                chatProvider.clearChat();
+                                if (!context.mounted) return;
+                                Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                    builder: (_) => LoginScreen(),
+                                  ),
+                                  (route) => false,
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                foregroundColor: Colors.white,
+                              ),
+                              child: Text("Logout"),
+                            ),
+                          ],
+                        ),
+                  );
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.logout_outlined), Text("Logout")],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
